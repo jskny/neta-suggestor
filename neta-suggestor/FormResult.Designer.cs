9e257ffba4749019be9c33aeab88e168efedceda
jskny <jskny.goo@gmail.com>
2017-12-16 22:46:56 +0900

    add make text functions.

--- neta-suggestor/FormResult.Designer.cs
+++ neta-suggestor/FormResult.Designer.cs
@@ -31,12 +31,18 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
+			this.resultLabel1 = new System.Windows.Forms.Label();
+			this.resultLabel2 = new System.Windows.Forms.Label();
+			this.resultLabel3 = new System.Windows.Forms.Label();
+			this.resultLabel4 = new System.Windows.Forms.Label();
+			this.resultLabel5 = new System.Windows.Forms.Label();
+			this.resultLabel6 = new System.Windows.Forms.Label();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -62,6 +68,12 @@
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.resultLabel6);
+			this.groupBox1.Controls.Add(this.resultLabel5);
+			this.groupBox1.Controls.Add(this.resultLabel4);
+			this.groupBox1.Controls.Add(this.resultLabel3);
+			this.groupBox1.Controls.Add(this.resultLabel2);
+			this.groupBox1.Controls.Add(this.resultLabel1);
 			this.groupBox1.Controls.Add(this.label7);
 			this.groupBox1.Controls.Add(this.label6);
 			this.groupBox1.Controls.Add(this.label5);
@@ -75,35 +87,15 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "提案内容";
 			// 
-			// label3
+			// label7
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.label3.Location = new System.Drawing.Point(6, 37);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(62, 22);
-			this.label3.TabIndex = 2;
-			this.label3.Text = "どこで";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.label4.Location = new System.Drawing.Point(6, 59);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(68, 22);
-			this.label4.TabIndex = 3;
-			this.label4.Text = "だれが";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.label5.Location = new System.Drawing.Point(6, 81);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(66, 22);
-			this.label5.TabIndex = 4;
-			this.label5.Text = "なにを";
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label7.Location = new System.Drawing.Point(6, 125);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(96, 22);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "どのように";
 			// 
 			// label6
 			// 
@@ -115,15 +107,35 @@
 			this.label6.TabIndex = 5;
 			this.label6.Text = "なぜ";
 			// 
-			// label7
+			// label5
 			// 
-			this.label7.AutoSize = true;
-			this.label7.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-			this.label7.Location = new System.Drawing.Point(6, 125);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(96, 22);
-			this.label7.TabIndex = 6;
-			this.label7.Text = "どのように";
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label5.Location = new System.Drawing.Point(6, 81);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(66, 22);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "なにを";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label4.Location = new System.Drawing.Point(6, 59);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(68, 22);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "だれが";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label3.Location = new System.Drawing.Point(6, 37);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(62, 22);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "どこで";
 			// 
 			// button1
 			// 
@@ -134,6 +146,66 @@
 			this.button1.Text = "閉じる";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// resultLabel1
+			// 
+			this.resultLabel1.AutoSize = true;
+			this.resultLabel1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel1.Location = new System.Drawing.Point(125, 21);
+			this.resultLabel1.Name = "resultLabel1";
+			this.resultLabel1.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel1.TabIndex = 7;
+			this.resultLabel1.Text = "resultLabel1";
+			// 
+			// resultLabel2
+			// 
+			this.resultLabel2.AutoSize = true;
+			this.resultLabel2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel2.Location = new System.Drawing.Point(125, 41);
+			this.resultLabel2.Name = "resultLabel2";
+			this.resultLabel2.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel2.TabIndex = 8;
+			this.resultLabel2.Text = "resultLabel2";
+			// 
+			// resultLabel3
+			// 
+			this.resultLabel3.AutoSize = true;
+			this.resultLabel3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel3.Location = new System.Drawing.Point(125, 63);
+			this.resultLabel3.Name = "resultLabel3";
+			this.resultLabel3.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel3.TabIndex = 9;
+			this.resultLabel3.Text = "resultLabel3";
+			// 
+			// resultLabel4
+			// 
+			this.resultLabel4.AutoSize = true;
+			this.resultLabel4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel4.Location = new System.Drawing.Point(125, 85);
+			this.resultLabel4.Name = "resultLabel4";
+			this.resultLabel4.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel4.TabIndex = 10;
+			this.resultLabel4.Text = "resultLabel4";
+			// 
+			// resultLabel5
+			// 
+			this.resultLabel5.AutoSize = true;
+			this.resultLabel5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel5.Location = new System.Drawing.Point(125, 107);
+			this.resultLabel5.Name = "resultLabel5";
+			this.resultLabel5.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel5.TabIndex = 11;
+			this.resultLabel5.Text = "resultLabel5";
+			// 
+			// resultLabel6
+			// 
+			this.resultLabel6.AutoSize = true;
+			this.resultLabel6.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.resultLabel6.Location = new System.Drawing.Point(125, 129);
+			this.resultLabel6.Name = "resultLabel6";
+			this.resultLabel6.Size = new System.Drawing.Size(102, 16);
+			this.resultLabel6.TabIndex = 12;
+			this.resultLabel6.Text = "resultLabel6";
 			// 
 			// FormResult
 			// 
@@ -167,5 +239,11 @@
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label resultLabel6;
+		private System.Windows.Forms.Label resultLabel5;
+		private System.Windows.Forms.Label resultLabel4;
+		private System.Windows.Forms.Label resultLabel3;
+		private System.Windows.Forms.Label resultLabel2;
+		private System.Windows.Forms.Label resultLabel1;
 	}
 }
--- neta-suggestor/FormResult.cs
+++ neta-suggestor/FormResult.cs
@@ -19,6 +19,74 @@ namespace neta_suggestor
 		}
 
 
+		protected Random m_rnd= null;
+		public string getTextWhen()
+		{
+			string[] tmp = {
+				"関心を持った時", "現在",
+				"１年前", "１年後",
+				"１０年前", "１０年後",
+				"小学生のころ", "中学生のころ", "高校生のころ", "大学生／専門学校生のころ",
+				"今とは別の物事が専門分野だった頃" };
+			return (tmp[this.m_rnd.Next() % tmp.Length]);
+		}
+
+		public string getTextWhere()
+		{
+			string[] tmp = {
+				"自宅で", "勤務先／学校で",
+				"カフェで", "飲食店で",
+				"ベットの上で", "電車の中で", "バスの中で",
+				"映画館の中で", "カラオケ店で",
+				"友人の家で", "路上で", "公園で", "図書館で", "橋の上で", "けもの道の中で",
+				"ショッピングセンターで", "スポーツセンターで",
+				"夢の中で"
+			};
+			return (tmp[this.m_rnd.Next() % tmp.Length]);
+		}
+
+		public string getTextWho()
+		{
+			string[] tmp = {
+				"あなたが",
+				"小学生のころの友人が",
+				"中学生のころの友人が",
+				"高校生のころの友人が",
+				"大学／専門学校生のころの友人が",
+				"学友・仕事仲間が",
+				"ご両親が", "おじいちゃんやおばあちゃんが",
+				"親戚が", "外国人が", "外国人留学生が",
+				"SNSでの知り合いが",
+				"５年前の自分が", "５年後の自分が"
+			};
+			return (tmp[this.m_rnd.Next() % tmp.Length]);
+		}
+
+		public string getTextWhy()
+		{
+			string[] tmp = {
+				"震災で", "交通事故で", "自然災害で", "感染症で",
+				"事件に巻き込まれて", "病で", "役割に没頭し過ぎて",
+				"誰かのことを顧みないで", "自己中心的で", "優先順位を正確に定めて",
+				"勘違いで", "真意を読み間違えて", "真価を図り損ねて",
+				"確約を得ないで", "十分確認をしないで", "大事なところを見落として"
+			};
+			return (tmp[this.m_rnd.Next() % tmp.Length]);
+		}
+
+		public string getTextWhat()
+		{
+			//this.m_rnd.Next()
+			string tmp = this.m_mainForm.getListBoxInterest().Items[this.m_rnd.Next()%this.m_mainForm.getListBoxInterest().Items.Count].ToString();
+			return (tmp);
+		}
+		public string getTextHow()
+		{
+			//this.m_rnd.Next()
+			string tmp = this.m_mainForm.getListBoxCan().Items[this.m_rnd.Next() % this.m_mainForm.getListBoxCan().Items.Count].ToString();
+			return (tmp);
+		}
+
 		public FormResult()
 		{
 			InitializeComponent();
@@ -31,7 +99,14 @@ namespace neta_suggestor
 
 		private void FormResult_Load(object sender, EventArgs e)
 		{
-			MessageBox.Show(this.m_mainForm.getListBoxCan().Items.Count.ToString(), "情報");
+			this.m_rnd = new Random();
+
+			this.resultLabel1.Text = this.getTextWhen();
+			this.resultLabel2.Text = this.getTextWhere();
+			this.resultLabel3.Text = this.getTextWho();
+			this.resultLabel4.Text = this.getTextWhat();
+			this.resultLabel5.Text = this.getTextWhy();
+			this.resultLabel6.Text = this.getTextHow();
 		}
 	}
 }
