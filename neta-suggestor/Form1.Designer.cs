42467a68e0fde54b8469f5fc346ceb060e434f7f
jskny <jskny.goo@gmail.com>
2017-12-16 16:13:59 +0900

    add main files.

---
+++ neta-suggestor/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace neta_suggestor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.初期化ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ヘルプHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.使い方UToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.バージョン情報VToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem,
+            this.ヘルプHToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileFToolStripMenuItem
+			// 
+			this.fileFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.初期化ToolStripMenuItem,
+            this.終了XToolStripMenuItem});
+			this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+			this.fileFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+			this.fileFToolStripMenuItem.Text = "ファイル(&F)";
+			// 
+			// 初期化ToolStripMenuItem
+			// 
+			this.初期化ToolStripMenuItem.Name = "初期化ToolStripMenuItem";
+			this.初期化ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.初期化ToolStripMenuItem.Text = "初期化";
+			// 
+			// 終了XToolStripMenuItem
+			// 
+			this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+			this.終了XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.終了XToolStripMenuItem.Text = "終了(&X)";
+			this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+			// 
+			// ヘルプHToolStripMenuItem
+			// 
+			this.ヘルプHToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.使い方UToolStripMenuItem,
+            this.バージョン情報VToolStripMenuItem});
+			this.ヘルプHToolStripMenuItem.Name = "ヘルプHToolStripMenuItem";
+			this.ヘルプHToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+			this.ヘルプHToolStripMenuItem.Text = "ヘルプ(&H)";
+			// 
+			// 使い方UToolStripMenuItem
+			// 
+			this.使い方UToolStripMenuItem.Name = "使い方UToolStripMenuItem";
+			this.使い方UToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+			this.使い方UToolStripMenuItem.Text = "使い方(&U)";
+			// 
+			// バージョン情報VToolStripMenuItem
+			// 
+			this.バージョン情報VToolStripMenuItem.Name = "バージョン情報VToolStripMenuItem";
+			this.バージョン情報VToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+			this.バージョン情報VToolStripMenuItem.Text = "バージョン情報(&V)";
+			this.バージョン情報VToolStripMenuItem.Click += new System.EventHandler(this.バージョン情報VToolStripMenuItem_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 461);
+			this.Controls.Add(this.menuStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ネタサジェスター";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 初期化ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ヘルプHToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 使い方UToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem バージョン情報VToolStripMenuItem;
+	}
+}
+
---
+++ neta-suggestor/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neta_suggestor
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+
+		private void バージョン情報VToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(
+				"Version beta.\n" +
+				"\n" +
+				"I have started blog site before 3 month.\n" +
+				"But, I can't earn article idea.\n" +
+				"So I start create an app that may suggest idea a little.\n" +
+				"I wish this app can complete.\n" +
+				"\n" +
+				"\n" +
+				"This App was created by jskny at 2017."
+				, "Version info", MessageBoxButtons.OK);
+		}
+	}
+}
